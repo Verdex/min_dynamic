cf27a058f2b214394ad27da5dd49279b3fb052b1
David L. Inman <davidlevere@gmail.com>
2017-12-08 20:04:59 -0500

    inc prog parser

--- Parser.cs
+++ Parser.cs
@@ -4,6 +4,21 @@ using Dalet.Lex;
 
 namespace Dalet.Parse
 {
+    publc interface Expr {}
+    public interface Stm {}
+    public class Var : Expr
+    {
+        public string Name { get; set; } 
+    }
+    public class VarName 
+    {
+    }
+    public class Foreach : Stm
+    {
+        public Expr SeqExpr { get; set; }
+        public VarName VarName { get; set; }
+    }
+
     public class Parser
     {
     }
